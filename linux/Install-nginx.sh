92fa7c6cf37e09a6f3f8f757c89c17b5b4da3b83
Ejiroghene Laurel Dafe <dafelaurele@gmail.com>
2023-12-16 11:23:07 +0000

    install nginx from source

--- linux/Install-nginx.sh
+++ linux/Install-nginx.sh
@@ -1,56 +1,83 @@
 #!/bin/bash
-# update package repository
-sudo apt update -y
 
-# install build essentials and dependencies
-sudo apt install build-essential libmaxminddb0 libmaxminddb-dev mmdb-bin checkinstall ufw -y
+# Set variables
+nginx_version="1.23.3"
+download_url="https://nginx.org/download/nginx-${nginx_version}.tar.gz"
 
-# create directory to store nginx source file
-sudo mkdir -p /opt/nginx && cd /opt/nginx
+# Update package list and install dependencies
+sudo apt update && sudo apt upgrade -y
+sudo apt install build-essential libpcre3 libpcre3-dev zlib1g libgd-dev zlib1g-dev libssl-dev cron -y
 
-# download nginx source code
-wget http://nginx.org/download/nginx-1.20.2.tar.gz
+# Create Nginx user and group
+sudo groupadd -r nginx && sudo useradd -r -g nginx nginx
 
-# extract source code
-tar -zxvf nginx-1.20.2.tar.gz
+# Download Nginx source code
+wget $download_url 
 
-# configure nginx
-cd nginx-1.20.2
-./configure --user=nginx --group=nginx --prefix=/opt/nginx --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock
+# Extract the downloaded archive
+tar -xzvf nginx-${nginx_version}.tar.gz 
 
-# build nginx
-make
+# Change directory to the extracted folder
+cd nginx-${nginx_version} 
 
-# install nginx
-sudo make install
+# Set error handling
+#sudo set -e
 
-# create an Nginx systemd service unit
-sudo cat << EOF >/etc/systemd/system/nginx.service
+# Configure Nginx with necessary options
+# Adjust this line with your preferred configuration options
+./configure --prefix=/var/www/html --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-pcre  --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-http_ssl_module --with-http_image_filter_module=dynamic --modules-path=/etc/nginx/modules --with-http_v2_module --with-stream=dynamic --with-http_addition_module --with-http_mp4_module
+
+# Build and install Nginx
+make && sudo make install |
+
+# Create a temporary directory
+temp_dir=$(mktemp -d)
+
+# Create the service file
+sudo bash -c cat << EOF > "$temp_dir/nginx.service"
 [Unit]
-Description=Nginx web server
-After=network.target
-
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network-online.target remote-fs.target nss-lookup.target
+Wants=network-online.target
+        
 [Service]
-Type=simple
+Type=forking
 User=nginx
 Group=nginx
-ExecStart=/opt/nginx/sbin/nginx
-ExecReload=/opt/nginx/sbin/nginx -s reload
+PIDFile=/var/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t
+ExecStart=/usr/sbin/nginx
+ExecReload=/usr/sbin/nginx -s reload
+ExecStop=/bin/kill -s QUIT $MAINPID
 PrivateTmp=true
-RuntimeDirectory=/opt/nginx
-
+Restart=on-failure
+RestartSec=3
+        
 [Install]
 WantedBy=multi-user.target
 EOF
 
-# reload systemd daemon
+# Set ownership of the service file
+sudo chown root:root "$temp_dir/nginx.service"
+
+# Copy the service file to the systemd directory
+sudo cp "$temp_dir/nginx.service" /etc/systemd/system/
+
+# Change Nginx binary ownership
+sudo chown -R nginx:nginx /usr/sbin/nginx
+
+# Reload systemd
 sudo systemctl daemon-reload
 
-# start Nginx service
+# Start Nginx service
 sudo systemctl start nginx
 
-# enable Nginx service to start at boot
+# Enable Nginx service to start at boot
 sudo systemctl enable nginx
 
+# Cleanup temporary directory
+rm -rf "$temp_dir"
+
+echo "Nginx installation completed successfully!"
+
 # add cron job to back up Nginx configuration files to home directory everyday
-echo "0 0 * * * sudo cp -rf /opt/nginx/conf/* ~/nginx-backup/" | sudo crontab -l -
