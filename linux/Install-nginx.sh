4058a329a7e37f04fbaaf4c8f877bbb149ee7268
Ejiroghene Laurel Dafe <99752279+EjiroLaurelD@users.noreply.github.com>
2023-12-16 17:30:33 +0100

    Update Install-nginx.sh

--- linux/Install-nginx.sh
+++ linux/Install-nginx.sh
@@ -8,33 +8,25 @@ download_url="https://nginx.org/download/nginx-${nginx_version}.tar.gz"
 sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential libpcre3 libpcre3-dev zlib1g libgd-dev zlib1g-dev libssl-dev cron -y
 
-# Create Nginx user and group
-sudo groupadd -r nginx && sudo useradd -r -g nginx nginx
-
 # Download Nginx source code
-wget $download_url 
+wget $download_url
 
 # Extract the downloaded archive
-tar -xzvf nginx-${nginx_version}.tar.gz 
+tar -xzvf nginx-${nginx_version}.tar.gz
 
 # Change directory to the extracted folder
-cd nginx-${nginx_version} 
-
-# Set error handling
-#sudo set -e
+cd nginx-${nginx_version}
 
 # Configure Nginx with necessary options
-# Adjust this line with your preferred configuration options
-./configure --prefix=/var/www/html --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-pcre  --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-http_ssl_module --with-http_image_filter_module=dynamic --modules-path=/etc/nginx/modules --with-http_v2_module --with-stream=dynamic --with-http_addition_module --with-http_mp4_module
+#./configure --prefix=/usr/local/nginx --with-http_ssl_module --with-http_v2_module
+./configure --user=nginx --group=nginx --prefix=/var/www/html --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-pcre  --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --with-http_ssl_module --with-http_image_filter_module=dynamic --modules-path=/etc/nginx/modules --with-http_v2_module --with-stream=dynamic --with-http_addition_module --with-http_mp4_module
 
 # Build and install Nginx
-make && sudo make install |
+make && sudo make install
 
-# Create a temporary directory
-temp_dir=$(mktemp -d)
-
-# Create the service file
-sudo bash -c cat << EOF > "$temp_dir/nginx.service"
+# create an Nginx systemd service unit
+#sudo cat << EOF > "$temp_dir/nginx.service"
+sudo bash -c 'cat > /etc/systemd/system/nginx.service' <<EOF
 [Unit]
 Description=The NGINX HTTP and reverse proxy server
 After=syslog.target network-online.target remote-fs.target nss-lookup.target
@@ -42,8 +34,6 @@ Wants=network-online.target
         
 [Service]
 Type=forking
-User=nginx
-Group=nginx
 PIDFile=/var/run/nginx.pid
 ExecStartPre=/usr/sbin/nginx -t
 ExecStart=/usr/sbin/nginx
@@ -57,27 +47,16 @@ RestartSec=3
 WantedBy=multi-user.target
 EOF
 
-# Set ownership of the service file
-sudo chown root:root "$temp_dir/nginx.service"
-
-# Copy the service file to the systemd directory
-sudo cp "$temp_dir/nginx.service" /etc/systemd/system/
-
-# Change Nginx binary ownership
+# Change Nginx user
 sudo chown -R nginx:nginx /usr/sbin/nginx
 
-# Reload systemd
+#sudo systemctl daemon-reload
+ 
+# reload systemd daemon
 sudo systemctl daemon-reload
 
-# Start Nginx service
+# start Nginx service
 sudo systemctl start nginx
 
-# Enable Nginx service to start at boot
+# enable Nginx service to start at boot
 sudo systemctl enable nginx
-
-# Cleanup temporary directory
-rm -rf "$temp_dir"
-
-echo "Nginx installation completed successfully!"
-
-# add cron job to back up Nginx configuration files to home directory everyday
